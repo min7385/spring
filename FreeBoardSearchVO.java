d4e8e505caaae6f45f7aa1f6c62ae5e2112139f2
min7385 <rkd7385@naver.com>
2024-10-08 12:51:54 +0900

    자유게시판(화면)
    드롭박스

---
+++ FreeBoardSearchVO.java
@@ -0,0 +1,42 @@
+package com.future.my.free.vo;
+
+import com.future.my.common.vo.PagingVO;
+
+public class FreeBoardSearchVO extends PagingVO{
+	
+	private String searchWord;		/*검색 키워드*/
+	private String searchType;		/*검색 조건 타입(T: 제목, W: 작성자, C: 내용*/
+	private String searchCategory;  /*게시판 카테고리*/
+	 
+	public String getSearchWord() {
+		return searchWord;
+	}
+	public void setSearchWord(String searchWord) {
+		this.searchWord = searchWord;
+	}
+	public String getSearchType() {
+		return searchType;
+	}
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+	public String getSearchCategory() {
+		return searchCategory;
+	}
+	public void setSearchCategory(String searchCategory) {
+		this.searchCategory = searchCategory;
+	}
+	
+	@Override
+	public String toString() {
+		return "FreeBoardSearchVO [searchWord=" + searchWord + ", searchType=" + searchType + ", searchCategory="
+				+ searchCategory + ", getSearchWord()=" + getSearchWord() + ", getSearchType()=" + getSearchType()
+				+ ", getSearchCategory()=" + getSearchCategory() + ", getCurPage()=" + getCurPage()
+				+ ", getRowSizePerPage()=" + getRowSizePerPage() + ", getPageSize()=" + getPageSize()
+				+ ", getTotalRowCount()=" + getTotalRowCount() + ", getFirstRow()=" + getFirstRow() + ", getLastRow()="
+				+ getLastRow() + ", getTotalPageCount()=" + getTotalPageCount() + ", getFirstPage()=" + getFirstPage()
+				+ ", getLastPage()=" + getLastPage() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+}
